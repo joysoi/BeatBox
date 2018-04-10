a900f09595ff54f3f67b588336eda990cf97d8ce
joysoi <nikola.joysoi@gmail.com>
2018-04-09 20:15:15 -0400

    orientation change

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -26,7 +26,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setRetainInstance(true);
         mBeatBox = new BeatBox(getActivity());
     }
 
@@ -37,6 +37,12 @@ public class BeatBoxFragment extends Fragment {
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.relase();
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder{
