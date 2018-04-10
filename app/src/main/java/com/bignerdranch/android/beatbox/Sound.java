e79301f090e33de0bee8e940dcb994610f9b6ea5
joysoi <nikola.joysoi@gmail.com>
2018-04-09 20:14:59 -0400

    model added

--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -4,6 +4,7 @@ package com.bignerdranch.android.beatbox;
 public class Sound {
     private String mAssetsPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetsPath) {
         mAssetsPath = assetsPath;
@@ -18,5 +19,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
