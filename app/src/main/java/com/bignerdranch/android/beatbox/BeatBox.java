2bf1ac49c1bba61c3d92a8102e1f3ca98d4950fb
joysoi <nikola.joysoi@gmail.com>
2018-04-09 20:15:49 -0400

    sound poll added

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -12,11 +15,14 @@ import java.util.List;
 public class BeatBox {
     public static final String TAG = "BeatBox";
     public static final String SOUND_FOLDERS = "sample_sounds";
+    public static final int MAX_SOUNDS = 5;
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSound();
     }
 
@@ -30,10 +36,33 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUND_FOLDERS + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUND_FOLDERS + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetsPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    public void relase(){
+        mSoundPool.release();
     }
 
     public List<Sound> getSounds() {
